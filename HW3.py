e19f1443b366c6868784aac738c5f9078f9b0df3
Sean Clough <stclough@uw.edu>
2024-11-08 20:13:14 -0800

    needed to change first part to solveivp instead of odeint

--- HW3.py
+++ HW3.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from scipy.sparse.linalg import eigs
 from scipy.integrate import solve_ivp
 
-def shoot2(x_vect, x, K, epsilon):
-    return [x_vect[1], (K*x**2-epsilon) * x_vect[0]]
+def shoot2(x, y, K, epsilon):
+    return [y[1], (K*x**2-epsilon) * y[0]]
 tol = 1e-4  # define a tolerance level 
 col = ['r', 'b', 'g', 'c', 'm']  # eigenfunc colors
 eps_start = 0.5
@@ -24,26 +24,29 @@ for modes in range(1, 6):  # begin mode loop
     deps = eps_start / 100  # default step size in beta
     for _ in range(1000):  # begin convergence loop for beta
         x0 = [A, A*np.sqrt(K*L**2-eps)] #initial conditions
-        y = odeint(shoot2, x0, xspan, args=(K,eps)) 
+        #y = odeint(shoot2, x0, xspan, args=(K,eps)) 
+        sol = solve_ivp(shoot2, [xspan[0], xspan[-1]], x0, t_eval=xspan, args=(K, eps))
+        y1 = sol.y[0]  
+        y2 = sol.y[1]
         # y = RK45(shoot2, xp[0], x0, xp[1], args=(n0,beta)) 
 
-        if abs(y[-1, 1] + np.sqrt(K*L**2-eps)*y[-1,0]) < tol:  # final condition
+        if abs(y2[-1] + np.sqrt(K*L**2-eps)*y1[-1]) < tol:  # final condition
             #print(eps)  # write out eigenvalue 
             eps_list.append(eps)
             #print(_)
             break  # get out of convergence loop
 
-        if (-1) ** (modes + 1) * (y[-1, 1] + np.sqrt(K*L**2-eps)*y[-1,0]) > 0:
+        if (-1) ** (modes + 1) * (y2[-1] + np.sqrt(K*L**2-eps)*y1[-1]) > 0:
             eps += deps
         else:
             eps -= deps / 2
             deps /= 2
 
     eps_start = eps + 0.01  # after finding eigenvalue, pick new start
-    norm = np.trapz(y[:, 0] * y[:, 0], xspan)  # calculate the normalization
+    norm = np.trapz(y1**2, xspan)  # calculate the normalization
     #plt.figure(modes)
     #plt.plot(xspan, y[:, 0] / np.sqrt(norm), col[modes - 1])  # plot modes
-    eig_func_list.append(abs(y[:, 0] / np.sqrt(norm)))
+    eig_func_list.append(abs(y1 / np.sqrt(norm)))
 #plt.show()
 A1 = abs(np.transpose(eig_func_list))
 A2 = eps_list
@@ -431,4 +434,19 @@ plt.show()
 other_one = np.genfromtxt('AMATH 481\AMATH-481-HW-Code\A11.csv', delimiter=',')
 error = A11-other_one
 print('error = ' + str(error))
-"""    
+"""
+"""
+other_one = np.genfromtxt('AMATH 481\AMATH-481-HW-Code\A1.csv', delimiter=',')
+error = A1-other_one
+y_min = -0.0007
+y_max = 0.0007
+y_ticks = np.linspace(y_min, y_max, 3)
+fig, axs = plt.subplots(2, 1, figsize=(10, 15))
+for i in range(6,8):
+    axs[i-6].plot(xspan, error[:,i-6])
+    #axs[i-6].set_ylim(y_min, y_max)  # Set the same y-axis limits for each subplot
+    #axs[i-6].set_yticks(y_ticks)
+    axs[i-6].set_title(f'Plot {i-6+1}')  # Optionally set a title for each subplot
+plt.subplots_adjust(hspace=0.5) 
+plt.show()
+"""     
